32015c35a3cb6c98f499d2d30bd7ff90ee34dbf7
melissakou <melissakou0420@gmail.com>
2021-10-18 16:08:49 +0000

    updata README.md

--- solutions/0122_Best_Time_to_Buy_and_Sell_Stock_II.py
+++ solutions/0122_Best_Time_to_Buy_and_Sell_Stock_II.py
@@ -3,29 +3,29 @@ from utils import timer
 class Solution:
 
     # Solution 1: Greedy: find peak & valley
-    @timer
-    def maxProfit(self, prices):
-        profit, buy = 0, None
-
-        for i in range(len(prices)):
-            if i == len(prices) - 1:
-                if buy is not None: profit += (prices[i] - buy)
-            elif prices[i] > prices[i+1] and buy is not None:
-                profit += (prices[i] - buy) ; buy = None
-            elif prices[i] < prices[i+1] and buy is None:
-                buy = prices[i]
-
-        return profit
-
-    # Solution 2: Greedy: every greedy
     # @timer
     # def maxProfit(self, prices):
-    #     profit = 0
-    #     for i in range(len(prices)-1):
-    #         slope = prices[i+1] - prices[i]
-    #         if slope > 0: profit += slope
-        
+    #     profit, buy = 0, None
+
+    #     for i in range(len(prices)):
+    #         if i == len(prices) - 1:
+    #             if buy is not None: profit += (prices[i] - buy)
+    #         elif prices[i] > prices[i+1] and buy is not None:
+    #             profit += (prices[i] - buy) ; buy = None
+    #         elif prices[i] < prices[i+1] and buy is None:
+    #             buy = prices[i]
+
     #     return profit
+
+    # Solution 2: Greedy: every greedy
+    @timer
+    def maxProfit(self, prices):
+        profit = 0
+        for i in range(len(prices)-1):
+            slope = prices[i+1] - prices[i]
+            if slope > 0: profit += slope
+        
+        return profit
 
 
 if __name__ == "__main__":
